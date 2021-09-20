d21c4d2617c0942a2fbbdfef62108cbadf900872
Mohammasd Ch <mamadbosqest@gmail.com>
2021-09-20 08:35:26 +0430

    initial commit

---
+++ src/music.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        source : "./music/Billie_Eilish_Therefore_I_Am_128.mp3",
+        photo : "./music/bilielish.jpg",
+        title : "Therefore I am "
+    },
+    {
+        source : "./music/Billie_Eilish-_Bad_Guy_320.mp3",
+        photo : "./music/bilielish.jpg",
+        title : "Bad_Guy"
+    },
+
+]
